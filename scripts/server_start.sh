d7f8fd1269fc1c59cca65f0be0bce3af9f68da00
Your Name <you@example.com>
2020-01-16 09:38:36 +0000

    adding java jar to server run

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-cd /home/ec2-user/springboot-codedeploy-script-sample/server
-sudo /usr/bin/java -jar -Dserver.port=80 \
-    *.jar > /dev/null 2> /dev/null < /dev/null &
+#cd /home/ec2-user/springboot-codedeploy-script-sample/server
+#sudo /usr/bin/java -jar -Dserver.port=80 \
+#    *.jar > /dev/null 2> /dev/null < /dev/null &
+
+java -jar /home/ec2-user/springboot-codedeploy-script-sample/target/hello-0.0.1-SNAPSHOT.jar
