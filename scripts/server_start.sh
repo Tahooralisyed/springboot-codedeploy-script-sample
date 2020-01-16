14ec5d0acfaaff319ab6c312ba051c3de8d58db4
Your Name <you@example.com>
2020-01-16 11:43:56 +0000

    tring new things

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -3,4 +3,6 @@
 #sudo /usr/bin/java -jar -Dserver.port=80 \
 #    *.jar > /dev/null 2> /dev/null < /dev/null &
 
-java -jar /home/ec2-user/springboot-codedeploy-script-sample/target/hello-0.0.1-SNAPSHOT.jar
+java -jar /home/ec2-user/hello-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+
+#java -jar -Dspring.profiles.active=dev /home/ec2-user/justiceleague-tracker-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
