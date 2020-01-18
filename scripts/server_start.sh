99933d6810a0c58b2bcba55d99ae3d95aa140a59
Your Name <you@example.com>
2020-01-18 06:22:17 +0000

    added few lines in start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,8 +1,2 @@
-#!/usr/bin/env bash
-#cd /home/ec2-user/springboot-codedeploy-script-sample/server
-#sudo /usr/bin/java -jar -Dserver.port=80 \
-#    *.jar > /dev/null 2> /dev/null < /dev/null &
-
+#!/bin/bash
 java -jar -Dspring.profiles.active=dev /home/ec2-user/hello-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
-
-#java -jar -Dspring.profiles.active=dev /home/ec2-user/justiceleague-tracker-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
